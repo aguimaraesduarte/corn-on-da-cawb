d9ecce79433a196a93c6c63db76659672ce79e63
Will Young <wjyoung@dons.usfca.edu>
2016-12-08 00:00:06 -0800

    commented file

--- imdb.py
+++ imdb.py
@@ -27,34 +27,71 @@ URL = "http://www.omdbapi.com/?"
 
 
 def get_single_factor(title, factor):
+    """
+    Returns the information on the factor for the single movie title
+    :param title: a movie title
+    :param factor: a single factor of interest
+    :return: data on the factor for the movie
+    """
     response = read_page_from_title(title)
     tree = gimme_xml(response)
     return tree.children[0].children[0][factor]
 
 
 def get_series_from_factor(titles, factor):
+    """
+    Builds a Pandas series of returned information for the single for all movies in titles
+    :param titles: a list of movie titles
+    :param factor: a single bit of information to pull
+    :return:
+    """
     factor_series = titles.apply(lambda title: get_single_factor(title, factor))
     return factor_series
 
 
 def gimme_json(response):
+    """
+    Given a urllib2 object returns a json tree.
+    :param response: urllib2 object
+    :return: untangle tree represenation of json
+    """
     data = json.loads(response.read())
     return json.dumps(data, indent=4)
 
 
 def gimme_xml(response):
+    """
+    Given a urllib2 object returns a untangle XML tree.
+    :param response: urllib2 object
+    :return: untangle tree represenation of xml
+    """
     data = response.read()
     data = untangle.parse(data)
     return data
 
 
 def read_page(url, opts):
-    # urlencode converts the dictionary to a list of x=y pairs
+    """
+    Request data and returns urllib2 object using given url template and options.
+
+    Note, urlencode converts the dictionary to a list of x=y pairs
+    :param url: url to request
+    :param opts: the options to append to the request
+    :return: urllib2 object
+    """
+
     query_url = url + urllib.urlencode(opts)
     return urllib2.urlopen(query_url)
 
 
 def read_page_from_title(title, url=URL, opts=XML_QUERY):
+    """
+    Request all information from IMDB on given title.
+    :param title: movie title to find
+    :param url: the url template string
+    :param opts: the url options
+    :return: urllib2 object representation of the page
+    """
     opts['t'] = title
     return read_page(url, opts)
 
