16010ff69bf479c69f2d899d1db674b843b48f7a
Will Young <wjyoung@dons.usfca.edu>
2016-12-07 23:59:54 -0800

    commented file

--- dict_reader.py
+++ dict_reader.py
@@ -4,20 +4,33 @@ The contents of this file are are first stored in a list-of-lists which is used 
 frame. This data frame is the merged with the IMDB 5000 movie data. This gives more information for use
 in modeling on each movie.
 
+NOTE: Some lines below are commented out to prevent over-writing files. Uncomment and rename as needed.
+
 """
 
 
 from datetime import datetime
 import pandas as pd
 
+IMDB_5000_FILE_NAME = "movie_metadata.csv"
 
 def create_df_from_dict_list(d_list):
+    """
+    Converts a list of dictionaries to a Pandas data frame.
+    :param d_list: a list of dictionaries
+    :return: Pandas data frame
+    """
     index = range(0, len(d_list))
     df = pd.DataFrame(d_list, index=index)
     return df
 
 
 def extract_month(date_string):
+    """
+    Extacts the month from a date string of a particualr format.
+    :param date_string: the date string formatted '%d %b %Y'
+    :return: month number (1 - 12)
+    """
     try:
         date_obj = datetime.strptime(date_string, '%d %b %Y')
         return date_obj.month
@@ -26,12 +39,21 @@ def extract_month(date_string):
 
 
 def read_format_imdb_5000():
-    df = pd.read_table("movie_metadata.csv", sep=",")
+    """
+    Reads and formats the original IMDB 5000 movie data stored in movie_metadata.csv
+    :return: Pandas data frame of the IMDB 5000 data
+    """
+    df = pd.read_table(IMDB_5000_FILE_NAME, sep=",")
     df['movie_title'] = df['movie_title'].apply(lambda x: x.replace('\xc2\xa0', ''))
     return df
 
 
 def read_file_to_dict_list(file_name):
+    """
+    Reads a file where each line is a dictionary into a list of dictionaries.
+    :param file_name: file name to open and read
+    :return: list of dictionaries
+    """
     d_list = []
     with open(file_name, 'r') as f:
         for line in f:
